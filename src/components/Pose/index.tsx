0b06089502e7b91a66b2672abcde835c69c68a0c
drpulpo <drpulpo@hotmail.com>
2024-03-01 16:49:23 -0600

    PoseContainer component added

---
+++ src/components/Pose/index.tsx
@@ -0,0 +1,124 @@
+import { useEffect, useRef, useState } from 'react';
+//import { Results, Hands, HAND_CONNECTIONS, VERSION } from '@mediapipe/hands';
+import { Pose, Results, POSE_CONNECTIONS, VERSION } from '@mediapipe/pose';
+
+import {
+  drawConnectors,
+  drawLandmarks,
+  Data,
+  lerp,
+} from '@mediapipe/drawing_utils';
+import './index.scss';
+
+const PoseContainer = () => {
+  const [inputVideoReady, setInputVideoReady] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+
+  const inputVideoRef = useRef<HTMLVideoElement | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const contextRef = useRef<CanvasRenderingContext2D | null>(null);
+
+  useEffect(() => {
+    if (!inputVideoReady) {
+      return;
+    }
+    if (inputVideoRef.current && canvasRef.current) {
+      console.log('rendering');
+      contextRef.current = canvasRef.current.getContext('2d');
+      const constraints = {
+        video: { width: { min: 1280 }, height: { min: 720 } },
+      };
+      navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        if (inputVideoRef.current) {
+          inputVideoRef.current.srcObject = stream;
+        }
+        sendToMediaPipe();
+      });
+
+      const pose = new Pose({
+        locateFile: (file) =>
+          `https://cdn.jsdelivr.net/npm/@mediapipe/pose@${VERSION}/${file}`,
+      });
+
+      pose.setOptions({
+        modelComplexity: 1,
+        minDetectionConfidence: 0.5,
+        minTrackingConfidence: 0.5,
+      });
+
+      pose.onResults(onResults);
+
+      const sendToMediaPipe = async () => {
+        if (inputVideoRef.current) {
+          if (!inputVideoRef.current.videoWidth) {
+            console.log(inputVideoRef.current.videoWidth);
+            requestAnimationFrame(sendToMediaPipe);
+          } else {
+            await pose.send({ image: inputVideoRef.current });
+            requestAnimationFrame(sendToMediaPipe);
+          }
+        }
+      };
+    }
+  }, [inputVideoReady]);
+
+  const onResults = (results: Results) => {
+    if (canvasRef.current && contextRef.current) {
+      setLoaded(true);
+
+      contextRef.current.save();
+      contextRef.current.clearRect(
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
+      contextRef.current.drawImage(
+        results.image,
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
+
+      if (results.poseLandmarks) {
+        for (let i = 0; i < results.poseLandmarks.length; i++) {
+          const landmarks = results.poseLandmarks;
+          drawConnectors(contextRef.current, landmarks, POSE_CONNECTIONS, {
+            color: '#FF0000'
+          });
+          drawLandmarks(contextRef.current, landmarks, {
+            color: '#FF0000',
+            fillColor: '#00FF00',
+            radius: (data: Data) => {
+              return lerp(data.from!.z!, -0.15, 0.1, 10, 1);
+            },
+          });
+        }
+      }
+
+      contextRef.current.restore();
+    }
+  };
+
+  return (
+    <div className="pose-container">
+      <video
+        autoPlay
+        ref={(el) => {
+          inputVideoRef.current = el;
+          setInputVideoReady(!!el);
+        }}
+      />
+      <canvas ref={canvasRef} width={1280} height={720} />
+      {!loaded && (
+        <div className="loading">
+          <div className="spinner"></div>
+          <div className="message">Loading</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PoseContainer;
